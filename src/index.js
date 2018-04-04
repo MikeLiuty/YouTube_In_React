d6eaba3ddbb908e1ea5532eadd8808783d80c4a5
Mikeliuty <mikeliuty@gmail.com>
2018-04-05 11:10:17 +1200

    Able to list all the videos.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar.js';
+import VideoList from './components/video_list.js';
 
 const API_KEY = 'AIzaSyC4D2DuvN1uihXMpCMYHQgJzFMbYDbGAsc';
 
-YTSearch({key:API_KEY, term:'surfboard'},function(data){console.log(data);})
-
 //Creat a new Component. This component should produce some HTML
-//Daily Commit request
-const App=() =>{
-  return (
-  <div>
-    <SearchBar />
-  </div>
-);
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={videos:[] };
+
+    YTSearch({key:API_KEY, term:'surfboard'},(videos)=>{
+      this.setState({videos});
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM);
